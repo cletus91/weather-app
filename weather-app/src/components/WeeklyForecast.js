9d0890288af1a47d0235dd39974299f5ece0e32d
Cletus <cletus6@gmail.com>
2020-11-24 23:10:44 -0500

    parse current localtime of a searched location

--- weather-app/src/components/WeeklyForecast.js
+++ weather-app/src/components/WeeklyForecast.js
@@ -2,38 +2,38 @@ import React from 'react';
 import moment from 'moment-timezone';
 
 require('dotenv').config();
-const WeeklyForecast = ({ forecast, day }) => {
+const WeeklyForecast = ({ forecast, day, localtime }) => {
 	console.log(forecast);
 	return (
 		<div className='hourly-forecast'>
 			{forecast
 				? forecast.map((currentDay) =>
 						currentDay.hour
-						.filter(hour => !moment(hour.time).isBefore(moment()))
-						.map((allHours) => {
-							let tempC = Math.floor(allHours.temp_c);
-							let time = moment(allHours.time).format('hh:mm A');
-							return (
-								<div
-									key={allHours.time}
-									className={`item ${day ? 'day' : 'night'}`}>
-									<div className='item-time'>{time}</div>
-									<div className='item-img'>
-										<img
-											className='item-img'
-											src={allHours.condition.icon}
-											alt={allHours.condition.icon}
-										/>{' '}
-										<span>
-											{allHours.chance_of_rain && allHours.chance_of_rain > 0
-												? allHours.chance_of_rain
-												: ''}
-										</span>
+							.filter((hour) => !moment(hour.time).isBefore(moment(localtime)))
+							.map((allHours) => {
+								let tempC = Math.floor(allHours.temp_c);
+								let time = moment(allHours.time).format('hh:mm A');
+								return (
+									<div
+										key={allHours.time}
+										className={`item ${day ? 'day' : 'night'}`}>
+										<div className='item-time'>{time}</div>
+										<div className='item-img'>
+											<img
+												className='item-img'
+												src={allHours.condition.icon}
+												alt={allHours.condition.icon}
+											/>{' '}
+											<span>
+												{allHours.chance_of_rain && allHours.chance_of_rain > 0
+													? allHours.chance_of_rain
+													: ''}
+											</span>
+										</div>
+										<div className='item-temp'>{tempC}°C</div>
 									</div>
-									<div className='item-temp'>{tempC}°C</div>
-								</div>
-							);
-						})
+								);
+							})
 				  )
 				: ''}
 		</div>
